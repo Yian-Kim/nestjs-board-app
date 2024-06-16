2f427364b6ae49a24866cb10b7dff73c82642a9d
Yian Kim <devyiankim@gmail.com>
2024-06-16 19:37:14 +0900

    feat: [#2] generate board service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards/boards.controller';
+import { BoardsService } from './boards/boards.service';
 
 @Module({
   imports: [],
   controllers: [BoardsController],
+  providers: [BoardsService],
 })
 export class AppModule {}
